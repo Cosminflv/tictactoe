884819d3499edbb47086fda1ad2499d6e98a1a90
Cosminflv <cosmin_popovici2012@yahoo.com>
2023-09-05 09:12:29 +0300

    added highlight on playerDisplay

--- lib/Widgets/player_card.dart
+++ lib/Widgets/player_card.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tic_tac_toe_flutter/cubit/tic_tac_toe_cubit.dart';
+import 'package:tic_tac_toe_flutter/cubit/tic_tac_toe_state.dart';
+import 'package:tic_tac_toe_lib/tic_tac_toe_lib.dart';
 
 class TurnDisplay extends StatelessWidget {
   final String player;
@@ -13,19 +17,40 @@ class TurnDisplay extends StatelessWidget {
     return SizedBox(
       width: MediaQuery.of(context).size.width * 0.25,
       height: 50,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).primaryColor,
-          borderRadius: BorderRadius.circular(25),
-        ),
-        child: Center(
-          child: Text(player,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.white,
-              )),
-        ),
+      child: BlocBuilder<TicTacToeCubit, TicTacToeState>(
+        builder: (context, state) {
+          if (player == 'X' && state.mTurn == Turn.crossTurn || player == 'O' && state.mTurn == Turn.zeroTurn) {
+            return Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(25),
+                  border: Border.all(color: Colors.cyan.shade50, width: 5)),
+              child: Center(
+                child: Text(player,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                    )),
+              ),
+            );
+          } else {
+            return Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Center(
+                child: Text(player,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                    )),
+              ),
+            );
+          }
+        },
       ),
     );
   }
