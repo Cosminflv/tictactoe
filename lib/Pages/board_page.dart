f219e965a7fa3fcf4f0c79d91661de8b2c7d4ca0
Cosminflv <cosmin_popovici2012@yahoo.com>
2023-09-05 08:56:35 +0300

    Added circle_progress in Widgets.

--- lib/Pages/board_page.dart
+++ lib/Pages/board_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tic_tac_toe_flutter/Pages/difficulty_page.dart';
+import 'package:tic_tac_toe_flutter/Widgets/circle_progress.dart';
 import 'package:tic_tac_toe_flutter/Widgets/player_card.dart';
 import 'package:tic_tac_toe_flutter/cubit/tic_tac_toe_cubit.dart';
 import 'package:tic_tac_toe_flutter/cubit/tic_tac_toe_state.dart';
@@ -140,41 +141,6 @@ class TicTacToeLayout extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class CircleProgress extends StatelessWidget {
-  const CircleProgress({
-    super.key,
-    this.player,
-  });
-
-  final Turn? player;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<TicTacToeCubit, TicTacToeState>(
-      builder: (context, state) {
-        if (state.mTurn == player) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CircularProgressIndicator(
-              value: state.mTimeLimited.inMilliseconds / 10000,
-              color: Theme.of(context).primaryColor,
-              backgroundColor: Colors.cyan.shade50,
-            ),
-          );
-        } else {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CircularProgressIndicator(
-              value: 0.0,
-              backgroundColor: Colors.cyan.shade50,
-            ),
-          );
-        }
-      },
     );
   }
 }
---
+++ lib/Widgets/circle_progress.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tic_tac_toe_flutter/cubit/tic_tac_toe_cubit.dart';
+import 'package:tic_tac_toe_flutter/cubit/tic_tac_toe_state.dart';
+import 'package:tic_tac_toe_lib/tic_tac_toe_lib.dart';
+
+class CircleProgress extends StatelessWidget {
+  const CircleProgress({
+    super.key,
+    this.player,
+  });
+
+  final Turn? player;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TicTacToeCubit, TicTacToeState>(
+      builder: (context, state) {
+        if (state.mTurn == player) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircularProgressIndicator(
+              value: state.mTimeLimited.inMilliseconds / 10000,
+              color: Theme.of(context).primaryColor,
+              backgroundColor: Colors.cyan.shade50,
+            ),
+          );
+        } else {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircularProgressIndicator(
+              value: 0.0,
+              backgroundColor: Colors.cyan.shade50,
+            ),
+          );
+        }
+      },
+    );
+  }
+}
